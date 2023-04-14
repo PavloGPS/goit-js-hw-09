a3cc65747219c45ef644e0f96dcac619c4a97504
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-14 13:37:11 +0300

    Update 01-color-switcher.js
    task 09_01 is solved

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtnEl=document.querySelector("[data-start]");
+const stopBtnEl=document.querySelector ("[data-stop]");
+let colorSwitch;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtnEl.addEventListener("click", onStartBtnClick);
+stopBtnEl.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick(){
+    colorSwitch = setInterval(()=>{
+        document.body.style.backgroundColor=getRandomHexColor();
+    },1000);
+    startBtnEl.disabled= true;
+    stopBtnEl.disabled= false;    
+}
+function onStopBtnClick() {
+    clearInterval(colorSwitch);
+    startBtnEl.disabled= false;
+    stopBtnEl.disabled= true; 
+  }
+    
+  stopBtnEl.disabled = true;
