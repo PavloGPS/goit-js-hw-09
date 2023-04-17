ddaa6c6f1ba3b13eaf99e49d4b9ed7cb11312e6f
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-17 18:37:44 +0300

    Update 02-timer.js
    1. makes start button  disable at first
    2. adds solvation of time that left till target date

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
 const refs={
     inputDateTimePickerEl:document.querySelector("#datetime-picker"),
     startBtnEl:document.querySelector("[data-start]"),
@@ -8,6 +9,7 @@ const refs={
     minutesTextValueEl:document.querySelector("[data-minutes]"),
     secondsTextValueEl:document.querySelector("[data-seconds]"),
 }
+
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -15,6 +17,18 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
       console.log(selectedDates[0]);
+      const targetDateInMs=selectedDates[0].getTime();
+      const currentDateInMs=Date.now().getTime();
+      const timeLeftInMs= targetDateInMs-currentDateInMs;
+
+      console.log('Current time in ms:', currentDateInMs);
+      console.log('Target time in ms:', targetDateInMs);
+    //   console.log('Target time in ms:', selectedDates[0].getTime());
+      console.log('Time left in ms:', (targetDateInMs-currentDateInMs));
+      refs.startBtnEl.disabled=false;
     },
   };
+
 flatpickr(refs.inputDateTimePickerEl, options);
+
+refs.startBtnEl.disabled=true;
