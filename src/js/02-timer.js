2b78ffe3bd2bf5f62469684cc3e4e4f48b8b6175
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-18 12:55:31 +0300

    task 09_2 solved
    notiflix.notyfy applied

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs={
     inputDateTimePickerEl:document.querySelector("#datetime-picker"),
@@ -8,8 +9,38 @@ const refs={
     hoursTextValueEl:document.querySelector("[data-hours]"),
     minutesTextValueEl:document.querySelector("[data-minutes]"),
     secondsTextValueEl:document.querySelector("[data-seconds]"),
+    timerEl:document.querySelector(".timer"),
 }
+const styleElMarkup = `
+<style>
+  .timer {
+    display: flex;
+    gap: 10px;
+  }
+  .field {
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    // outline: 2px ridge rgba(170, 50, 220, .6);
+    // border-radius: 10px;
+  } 
+  .value {
+    display: flex;
+    justify-content: center;
+    font-size: 50px;
+    text-transform: uppercase;
+    // border-bottom: thick double #32a1ce;
+  }
+  .label {
+    display: flex;
+    justify-content: center;
+    font-size: 15px;
+  }
+</style>`;
 
+refs.timerEl.insertAdjacentHTML('beforebegin',styleElMarkup);
+
+let targetDateInMs=null;
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -17,18 +48,65 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
       console.log(selectedDates[0]);
-      const targetDateInMs=selectedDates[0].getTime();
-      const currentDateInMs=Date.now().getTime();
-      const timeLeftInMs= targetDateInMs-currentDateInMs;
-
-      console.log('Current time in ms:', currentDateInMs);
-      console.log('Target time in ms:', targetDateInMs);
-    //   console.log('Target time in ms:', selectedDates[0].getTime());
-      console.log('Time left in ms:', (targetDateInMs-currentDateInMs));
-      refs.startBtnEl.disabled=false;
+      targetDateInMs=selectedDates[0].getTime();
+      
+      if (timeLeftInMs()<0){
+        // window.alert("Please choose a date in the future");
+        // Notify.warning('Please choose a date in the future');
+        Notify.failure('Please choose a date in the future');
+      } 
+      else{      
+        refs.startBtnEl.disabled=false;
+      }      
     },
   };
 
 flatpickr(refs.inputDateTimePickerEl, options);
 
 refs.startBtnEl.disabled=true;
+
+function timeLeftInMs(){return targetDateInMs-Date.now()}
+
+function addLeadingZero(value){
+  return String(value).padStart(2,0);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+refs.startBtnEl.addEventListener("click",onStartBtnClick)
+function onStartBtnClick(){
+  timerId = setInterval(()=>{
+    const timerValueObj= convertMs(timeLeftInMs());
+    updateTimerValue(timerValueObj);      
+    if(timeLeftInMs()<1000){
+      refs.inputDateTimePickerEl.disabled= false;
+      Notify.success('😎 Time is up!')
+      clearInterval(timerId);      
+    }
+  },1000);
+  refs.startBtnEl.disabled= true;    
+  refs.inputDateTimePickerEl.disabled= true;    
+}
+
+function updateTimerValue({days,hours,minutes,seconds}){
+  refs.daysTextValueEl.textContent=addLeadingZero(days);
+  refs.hoursTextValueEl.textContent=addLeadingZero(hours);
+  refs.minutesTextValueEl.textContent=addLeadingZero(minutes);
+  refs.secondsTextValueEl.textContent=addLeadingZero(seconds);
+}
